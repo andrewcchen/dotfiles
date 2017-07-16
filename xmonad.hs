820f43326aa7b24a672d8362401d653d5b2a0e12
Andrew Chen <andrew.chuanye.chen@gmail.com>
2017-07-16 22:03:45 +1200

    xmonad.hs: tabs to spaces

--- xmonad.hs
+++ xmonad.hs
@@ -16,28 +16,28 @@ import System.IO (hPutStrLn)
 import Text.Printf (printf)
 
 main = do
-	xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-	mapM spawn myExecute
-	xmonad $ ewmh $ def
-		{ handleEventHook = myHandleEventHook
-		, layoutHook = myLayoutHook
-		, logHook = dynamicLogWithPP xmobarPP
-			{ ppOutput = hPutStrLn xmproc
-			, ppTitle = xmobarColor "green" "" . shorten 50
-			}
-		, manageHook = myManageHook
-		, modMask = myModMask
-		, terminal = "konsole --workdir ~"
-		, workspaces = myWorkspaces
-		} `additionalKeys` myAdditionalKeys
+    xmproc <- spawnPipe "xmobar ~/.xmobarrc"
+    mapM spawn myExecute
+    xmonad $ ewmh $ def
+        { handleEventHook = myHandleEventHook
+        , layoutHook = myLayoutHook
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppTitle = xmobarColor "green" "" . shorten 50
+            }
+        , manageHook = myManageHook
+        , modMask = myModMask
+        , terminal = "konsole --workdir ~"
+        , workspaces = myWorkspaces
+        } `additionalKeys` myAdditionalKeys
 
 myRunOnce exe = printf fmt exe exe
-	where fmt = "if [ -z \"$(pidof %s)\" ]; then exec %s; fi"
+    where fmt = "if [ -z \"$(pidof %s)\" ]; then exec %s; fi"
 
 myExecute =
-	[ "killall trayer; exec trayer --edge top --align left --expand true --distance 1230 --distancefrom left --widthtype pixel --width 136 --height 16 --transparent true --alpha 0 --tint 0x00000000 --SetDockType true --SetPartialStrut true"
-	, myRunOnce "keepassxc"
-	]
+    [ "killall trayer; exec trayer --edge top --align left --expand true --distance 1230 --distancefrom left --widthtype pixel --width 136 --height 16 --transparent true --alpha 0 --tint 0x00000000 --SetDockType true --SetPartialStrut true"
+    , myRunOnce "keepassxc"
+    ]
 
 myHandleEventHook = docksEventHook
                 <+> fullscreenEventHook
@@ -55,9 +55,6 @@ myLayoutHook = lessBorders OnlyFloat
 
 myManageHook = manageDocks
            <+> (appName =? "trayer" --> doIgnore)
-           -- workaround for CKAN freezing xmonad
-           -- <+> (("CKAN " `isPrefixOf`) <$> title --> doIgnore)
-           -- <+> (className =? "Gimp" --> doFloat)
            <+> (title =? "Auto-Type - KeePassXC" --> doFloat)
            -- KeePassXC starts with this title, before changing it immediately
            <+> (title =? "KeePassXC" --> doShift "F12")
@@ -71,39 +68,39 @@ myModMask = mod1Mask -- Alt
 myWorkspaces = map show [1..9] ++ (map snd myExtraWorkspaces)
 
 myExtraWorkspaces =
-	[ (xK_0, "10")
-	, (xK_minus, "11")
-	, (xK_equal, "12")
-	, (xK_F1, "F1")
-	, (xK_F2, "F2")
-	, (xK_F3, "F3")
-	, (xK_F4, "F4")
-	, (xK_F5, "F5")
-	, (xK_F6, "F6")
-	, (xK_F7, "F7")
-	, (xK_F8, "F8")
-	, (xK_F9, "F9")
-	, (xK_F10, "F10")
-	, (xK_F11, "F11")
-	, (xK_F12, "F12")
-	]
+    [ (xK_0, "10")
+    , (xK_minus, "11")
+    , (xK_equal, "12")
+    , (xK_F1, "F1")
+    , (xK_F2, "F2")
+    , (xK_F3, "F3")
+    , (xK_F4, "F4")
+    , (xK_F5, "F5")
+    , (xK_F6, "F6")
+    , (xK_F7, "F7")
+    , (xK_F8, "F8")
+    , (xK_F9, "F9")
+    , (xK_F10, "F10")
+    , (xK_F11, "F11")
+    , (xK_F12, "F12")
+    ]
 
 myFocusedDoFullFloat windowset =
-	case W.stack $ W.workspace $ W.current windowset of
-		Nothing -> windowset
-		Just stack -> W.float (W.focus stack) (W.RationalRect 0 0 1 1) windowset
+    case W.stack $ W.workspace $ W.current windowset of
+        Nothing -> windowset
+        Just stack -> W.float (W.focus stack) (W.RationalRect 0 0 1 1) windowset
 
 myAdditionalKeys =
-	[ ((myModMask .|. shiftMask, xK_q), return ())
-	, ((myModMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; systemctl suspend")
-	, ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10")
-	, ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
-	, ((myModMask, xK_f), sendMessage ToggleLayout)
-	, ((myModMask .|. shiftMask, xK_f), windows myFocusedDoFullFloat)
-	] ++ [
-		((myModMask, key), (windows $ W.greedyView ws))
-		| (key, ws) <- myExtraWorkspaces
-	] ++ [
-		((myModMask .|. shiftMask, key), (windows $ W.shift ws))
-		| (key, ws) <- myExtraWorkspaces
-	]
+    [ ((myModMask .|. shiftMask, xK_q), return ())
+    , ((myModMask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; systemctl suspend")
+    , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight -inc 10")
+    , ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -dec 10")
+    , ((myModMask, xK_f), sendMessage ToggleLayout)
+    , ((myModMask .|. shiftMask, xK_f), windows myFocusedDoFullFloat)
+    ] ++ [
+        ((myModMask, key), (windows $ W.greedyView ws))
+        | (key, ws) <- myExtraWorkspaces
+    ] ++ [
+        ((myModMask .|. shiftMask, key), (windows $ W.shift ws))
+        | (key, ws) <- myExtraWorkspaces
+    ]
